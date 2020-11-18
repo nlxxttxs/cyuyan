2495befab841bfdc7c7f20d60c16822b024105f6
nlxxttxs <74277639+nlxxttxs@users.noreply.github.com>
2020-11-18 08:58:53 +0800

    Create 20201118001.习题3.c

---
+++ 20201118001.习题3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void main()
+{
+int a=3,b=4,c=5,x,y;
+if(!(a+b)+c-1&&b+c/2)
+{
+	printf("1\n");
+}
+else printf("0\n");
+if(!(x=a)&&(y=b)&&0)
+{
+	printf("1\n");
+}
+else printf("0\n");
+if(!(a>b)&&!c||1)
+{
+	printf("1\n");
+}
+else printf("0\n");
+if(a||b+c&&b-c)
+{
+	printf("1\n");
+}
+else printf("0\n");
+if(a+b>c&&b==c)
+{
+	printf("1\n");
+}
+else printf("0\n");
+}

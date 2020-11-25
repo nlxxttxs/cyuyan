2eae3cc49a1994674899f8aa29236d0f5a1544f5
nlxxttxs <74277639+nlxxttxs@users.noreply.github.com>
2020-11-25 09:45:28 +0800

    Create 112502.QS排名查询.app.c

---
+++ 112502.QS排名查询.app.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+int a;
+scanf("%d",&a);
+if(a==1){printf("清华大学");}
+else if(a==2){printf("北京大学");}
+else if(a==3){printf("中国科学技术大学");}
+else if(a==4){printf("浙江大学");}
+else if(a==5){printf("复旦大学");}
+else if(a==6){printf("南京大学");}
+else if(a==7){printf("上海交通大学");}
+else if(a==8){printf("中山大学");}
+else if(a==9){printf("北京师范大学");}
+else if(a==10){printf("华中科技大学");}
+else {printf("老铁的排名太靠后了，请输入1-10");}
+}

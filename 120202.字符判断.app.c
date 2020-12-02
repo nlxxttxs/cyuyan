ae87d50be2180e656f8c1a50381637912067bffc
nlxxttxs <74277639+nlxxttxs@users.noreply.github.com>
2020-12-02 09:33:32 +0800

    Update 120202.字符判断.app.c

--- 120202.字符判断.app.c
+++ 120202.字符判断.app.c
@@ -3,11 +3,11 @@ int main(){
 char q;
 scanf("%c",&q);
 switch(q){
-case 'w':printf("你在点击上键\n");break;
+	case 'w':printf("你在点击上键\n");break;
 	case 'd':printf("你在点击右键\n");break;
-		case 'a':printf("你在点击左键\n");break;
-			case 's':printf("你在点击下键\n");break;
-			default:printf("你点了个寂寞\n");
+	case 'a':printf("你在点击左键\n");break;
+	case 's':printf("你在点击下键\n");break;
+	default:printf("you did\n");
 }
 return 0;
 }

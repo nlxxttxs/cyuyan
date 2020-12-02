774eb02d4673b73a23108b5a13e8d365982a97e4
nlxxttxs <74277639+nlxxttxs@users.noreply.github.com>
2020-12-02 16:54:41 +0800

    Update 120203.秒转时分秒.app.c

--- 120203.秒转时分秒.app.c
+++ 120203.秒转时分秒.app.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+ #include <stdio.h>
 int main(){
 int i;
 scanf("%d",&i);
 if(i<=86400){
-printf("%d时:%d分:%d秒\n",i/3600,i%3600/60,i%3600%60);
+printf("%02d:%02d:%02d\n",i/3600,i%3600/60,i%3600%60);
 }
 else {
 	printf("你输入了一个过大的值\n");}

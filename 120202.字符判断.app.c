b9566b1f0e930354014c20256e9a90bbe8eb0ac8
nlxxttxs <74277639+nlxxttxs@users.noreply.github.com>
2020-12-02 09:32:33 +0800

    Create 120202.字符判断.app.c

---
+++ 120202.字符判断.app.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+char q;
+scanf("%c",&q);
+switch(q){
+case 'w':printf("你在点击上键\n");break;
+	case 'd':printf("你在点击右键\n");break;
+		case 'a':printf("你在点击左键\n");break;
+			case 's':printf("你在点击下键\n");break;
+			default:printf("你点了个寂寞\n");
+}
+return 0;
+}

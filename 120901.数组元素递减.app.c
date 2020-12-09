ab75392dc0976c0b763e8442f69a6065cd4f9785
nlxxttxs <74277639+nlxxttxs@users.noreply.github.com>
2020-12-09 09:03:28 +0800

    Create 120901.数组元素递减.app.c

---
+++ 120901.数组元素递减.app.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+int i,t,w;int q=90;
+int a[9][10]={
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {},
+    {}
+};
+     for(i=0;i<9;i++){
+        for(t=0;t<10;t++){
+                a[i][t]=0;
+            printf("%d ",a[i][t]);
+        }printf("\n");
+     }
+          for(i=0;i<9;i++){
+        for(t=0;t<10;t++){
+            a[i][t]=q--;
+            printf("%d ",a[i][t]);
+        }printf("\n");
+     }
+return 0;
+}

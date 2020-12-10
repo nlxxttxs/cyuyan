7a621b186c093d138b0fa73f903c74fbf0a6e2c3
nlxxttxs <74277639+nlxxttxs@users.noreply.github.com>
2020-12-10 20:15:39 +0800

    Update 120906.吃货给我安排.app.c

--- 120906.吃货给我安排.app.c
+++ 120906.吃货给我安排.app.c
@@ -2,15 +2,11 @@
 #include<string.h>
 int main(){
 printf("                                                欢迎使用吃货小程序\n");
-char a[10];
-char b[10]="春节";
-char c[10]="端午节";
-char d[10]="中秋节";
-char e[10]="元宵节";
+char a[];
 scanf("%s",&a);
-if(strcmp(a,b)==0){printf("给我安排饺子");}
-if(strcmp(a,c)==0){printf("给我安排粽子");}
-if(strcmp(a,d)==0){printf("给我安排月饼");}
-if(strcmp(a,e)==0){printf("给我安排元宵");}
+if(strcmp(a,"春节")==0){printf("给我安排饺子");}
+else if(strcmp(a,"端午节")==0){printf("给我安排粽子");}
+else if(strcmp(a,"中秋节")==0){printf("给我安排月饼");}
+else if(strcmp(a,"元宵节")==0){printf("给我安排元宵");}
 return 0;
 }

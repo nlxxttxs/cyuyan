45fed1e8e66480e4a498811dfc8e71b07c1f71b8
nlxxttxs <74277639+nlxxttxs@users.noreply.github.com>
2020-12-10 20:24:20 +0800

    Update 120906.吃货给我安排.app.c

--- 120906.吃货给我安排.app.c
+++ 120906.吃货给我安排.app.c
@@ -2,11 +2,12 @@
 #include<string.h>
 int main(){
 printf("                                                欢迎使用吃货小程序\n");
-char a[];
+char a[50];
 scanf("%s",&a);
 if(strcmp(a,"春节")==0){printf("给我安排饺子");}
 else if(strcmp(a,"端午节")==0){printf("给我安排粽子");}
 else if(strcmp(a,"中秋节")==0){printf("给我安排月饼");}
 else if(strcmp(a,"元宵节")==0){printf("给我安排元宵");}
+else {printf("你的输入错误");}
 return 0;
 }

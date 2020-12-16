4e1cfa053f655ea28308fccc7a6c133a8b100feb
nlxxttxs <74277639+nlxxttxs@users.noreply.github.com>
2020-12-16 09:37:08 +0800

    Update 121602.常规求和.app.c

--- 121602.常规求和.app.c
+++ 121602.常规求和.app.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main (){
-int i,n=0;
+int i,n=1,sum=0;
 scanf("%d",&i);
-if(n <= i){
-    printf("%d",n++);
-}
+for(n = 0;n<=i;n++){
+    sum=sum+n;
+} printf("%d",sum);
 return 0;
 }
